aa8712172b069b7d03d675d6005a011ce7f2a6ed
Ketal <ketal@qq.com>
2021-04-21 22:51:41 +0800

    Update MainHook

--- app/src/main/java/com/ketal/bcrfullscreen/MainHook.java
+++ app/src/main/java/com/ketal/bcrfullscreen/MainHook.java
@@ -1,33 +1,54 @@
 package com.ketal.bcrfullscreen;
 
-import android.app.Activity;
+import static android.view.WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import static de.robv.android.xposed.XposedHelpers.findClass;
+import static de.robv.android.xposed.XposedHelpers.getObjectField;
+
+import android.annotation.TargetApi;
 import android.os.Build;
 import android.view.WindowManager;
-
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
-
-import static android.view.WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainHook implements IXposedHookLoadPackage {
+    private static final List<String> PKGS = new ArrayList<String>();
+
+    static {
+        PKGS.add("com.bilibili.priconne");
+        PKGS.add("jp.co.cygames.princessconnectredive");
+    }
+
     @Override
-    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P)
+            return;
         if (!lpparam.packageName.equals(lpparam.processName))
             return;
-        if (!lpparam.packageName.equals("com.bilibili.priconne"))
+        if (!lpparam.packageName.equals("android"))
             return;
-        XposedBridge.hookMethod(Activity.class.getDeclaredMethod("onResume"), new XC_MethodHook() {
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) {
-                Activity act = (Activity) param.thisObject;
-                WindowManager.LayoutParams attributes = act.getWindow().getAttributes();
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                    attributes.layoutInDisplayCutoutMode = LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
-                    act.getWindow().setAttributes(attributes);
+        try {
+            Class<?> windowsState =
+                    findClass("com.android.server.wm.WindowState", lpparam.classLoader);
+            XC_MethodHook hook = new XC_MethodHook() {
+                @TargetApi(Build.VERSION_CODES.P)
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) {
+                    WindowManager.LayoutParams attrs =
+                            (WindowManager.LayoutParams) getObjectField(param.thisObject, "mAttrs");
+                    if (PKGS.contains(attrs.packageName)) {
+                        //XposedBridge.log("Change Window From" + attrs.packageName);
+                        attrs.layoutInDisplayCutoutMode = LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+                    }
                 }
-            }
-        });
+            };
+            findAndHookMethod(windowsState, "getAttrs", hook);
+        } catch (Throwable t) {
+            XposedBridge.log(t);
+        }
     }
 }
